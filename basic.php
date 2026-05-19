50daaafb1ead5f099053bf2aa6ae4888c5c474a2
madkrai87 <ahmadmuhamad87@gmail.com>
2026-05-19 06:47:04 +0000

    update php file

---
+++ basic.php
@@ -0,0 +1,16 @@
+<?php
+//Variables lways start with $
+$name="Alice";
+$age="25";
+$isAdmin= true;
+
+//Output to page
+echo "Hello".$name;
+
+//Conditionals
+if ($age>=18){echo "Adult";}
+else{echo "Minor"}
+
+//Arrays
+$skills=["HTML","CSS"]
+foreach($skilss as $s){echo"<li>$s</li>;}
